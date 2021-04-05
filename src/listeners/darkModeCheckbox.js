a2496afd0ec7ff7c8772896a79622b564c99d2a9
David Griswold <david.griswold@graded.br>
2021-04-05 10:20:46 -0300

    fixed ref issue and darkmode bug

--- src/listeners/darkModeCheckbox.js
+++ src/listeners/darkModeCheckbox.js
@@ -1,12 +1,15 @@
 var checkbox = document.querySelector("#darkmodecheckbox");
-if (localStorage.getItem("darkMode") && localStorage.getItem("darkMode") == "true") {
+if (localStorage.getItem("darkMode") === "true") {
   checkbox.checked = true;
   document.querySelector("html").classList.remove("light");
   document.querySelector("html").classList.add("dark");
+} else {
+  checkbox.checked = false;
+  document.querySelector("html").classList.add("light");
+  document.querySelector("html").classList.remove("dark");
 }
 
 checkbox.addEventListener("input", (e) => {
-  console.log("run");
   if (checkbox.checked === true) {
     localStorage.setItem("darkMode", "true");
     document.querySelector("html").classList.remove("light");
